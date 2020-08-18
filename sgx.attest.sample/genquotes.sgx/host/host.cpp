4905b627e8a2be0e201879e633d309f679933302
Olga Kroshkina <olkroshk@microsoft.com>
2020-08-18 09:01:50 -0700

    some temporary changes

--- sgx.attest.sample/genquotes.sgx/host/host.cpp
+++ sgx.attest.sample/genquotes.sgx/host/host.cpp
@@ -41,17 +41,10 @@
 #include "sgx_eid.h"     /* sgx_enclave_id_t */
 #include "sgx_urts.h"
 #include "enclave_u.h"
-#include "helpers.h"
 
-#define MAX_PATH FILENAME_MAX
-#define TOKEN_FILENAME  "enclave.token"
-
-static bool enable_verbose = true;//false;
-static sgx_enclave_id_t global_eid = 0;
-
-void myprintf(const char *format, ...);
-void print_error_message(sgx_status_t ret);
-int initialize_enclave(const char* enclave_path);
+bool create_app_enclave_report(const char* enclave_path,
+                               sgx_target_info_t qe_target_info,
+                               sgx_report_t *app_report);
 
 int SGX_CDECL main(int argc, char *argv[])
 {
@@ -63,158 +56,131 @@ int SGX_CDECL main(int argc, char *argv[])
         return 1;
     }
 
-    sgx_enclave_id_t enclave_id = 0; 
+    int ret = 0;
 
-    myprintf("Host: Creating the enclave\n");
+    printf("\nStep1: Call sgx_qe_get_target_info: ");
+    sgx_target_info_t qe_target_info;
+    quote3_error_t qe3_ret = sgx_qe_get_target_info(&qe_target_info);
+    if (SGX_QL_SUCCESS != qe3_ret) {
+        printf("Error in sgx_qe_get_target_info. 0x%04x\n", qe3_ret);
+        return -1;
+    }
+    printf("succeed!\n");
 
-    /* Initialize the enclave */
-    if(initialize_enclave(argv[1]) < 0){
-        printf("Epic fail ...\n");
-        return -1; 
+    printf("\nStep2: Call create_app_report: ");
+    sgx_report_t app_report;
+    if(true != create_app_enclave_report(argv[1], qe_target_info, &app_report)) {
+        printf("Call to create_app_report() failed\n");
+        return -1;
+    }
+    printf("succeed!\n");
+
+    printf("\nStep3: Call sgx_qe_get_quote_size: ");
+    uint32_t quote_size = 0;
+    qe3_ret = sgx_qe_get_quote_size(&quote_size);
+    if (SGX_QL_SUCCESS != qe3_ret) {
+        printf("Error in sgx_qe_get_quote_size. 0x%04x\n", qe3_ret);
+        return -1;
+    }
+    printf("succeed!\n");
+
+    printf("\nAllocate quote_buffer: ");
+    uint8_t* p_quote_buffer = (uint8_t*)malloc(quote_size);
+    if (NULL == p_quote_buffer) {
+        printf("Couldn't allocate quote_buffer\n");
+        if (NULL != p_quote_buffer) {
+            free(p_quote_buffer);
+        }
+        return -1;
+    }
+    memset(p_quote_buffer, 0, quote_size);
+    printf("succeed!\n");
+
+    printf("\nStep4: Call sgx_qe_get_quote: ");
+    qe3_ret = sgx_qe_get_quote(&app_report,
+                               quote_size,
+                               p_quote_buffer);
+    if (SGX_QL_SUCCESS != qe3_ret) {
+        printf( "Error in sgx_qe_get_quote. 0x%04x\n", qe3_ret);
+        if (NULL != p_quote_buffer) {
+            free(p_quote_buffer);
+        }
+        return -1;
+    }
+    printf("succeed!\n");
+
+    sgx_quote3_t *p_quote = (_sgx_quote3_t*)p_quote_buffer;
+    sgx_ql_ecdsa_sig_data_t *p_sig_data = (sgx_ql_ecdsa_sig_data_t *)p_quote->signature_data;
+    sgx_ql_auth_data_t *p_auth_data = (sgx_ql_auth_data_t*)p_sig_data->auth_certification_data;
+    sgx_ql_certification_data_t *p_cert_data =
+            (sgx_ql_certification_data_t *)((uint8_t *)p_auth_data + sizeof(*p_auth_data) + p_auth_data->size);
+
+    printf("cert_key_type = 0x%x\n", p_cert_data->cert_key_type);
+
+    printf("\nStep5: Sate quote to JSON");
+    FILE *fp = fopen("../genquotes/quotes/enclave1.info.debug.json", "w");
+    fprintf(fp, "%s\n", "{");
+    fprintf(fp, "  \"Type\": %d,\n", (int)2); // In oe sdk enclave type 2 means OE_ENCLAVE_TYPE_SGX: https://github.com/openenclave/openenclave/blob/3e15573418caed43f9094ff8aec36cdde4f278f7/include/openenclave/bits/types.h#L127
+    fprintf(fp, "  \"MrEnclaveHex\": \"%s\",\n", format_hex_buffer(hex_buffer, hex_buffer_size, app_report.body.mr_enclave.m, 32));
+    fprintf(fp, "  \"MrSignerHex\": \"%s\",\n", format_hex_buffer(hex_buffer, hex_buffer_size, app_report.body.mr_signer.m, 32));
+    fprintf(fp, "  \"ProductIdHex\": \"%u\",\n", (uint16_t)app_report.body.isv_prod_id);
+    fprintf(fp, "  \"SecurityVersion\": %u,\n", (int)app_report.body.isv_prod_id);
+    fprintf(fp, "  \"Attributes\": %u,\n", (uint64_t)app_report.body.attributes.flags);
+    //fprintf(fp, "  \"QuoteHex\": \"%s\",\n", format_hex_buffer(hex_buffer, hex_buffer_size, p_quote_buffer, quote_size));
+    //fprintf(fp, "  \"EnclaveHeldDataHex\": \"%s\"\n", format_hex_buffer(hex_buffer, hex_buffer_size, (uint8_t*)p_auth_data, sizeof(*p_auth_data)));
+    fprintf(fp, "%s\n", "}");
+    fclose(fp);
+
+    if (NULL != p_quote_buffer) {
+        free(p_quote_buffer);
+    }
+    return ret;
+}
+
+bool create_app_enclave_report(const char* enclave_path,
+                               sgx_target_info_t qe_target_info,
+                               sgx_report_t *app_report)
+{
+    bool ret = true;
+    uint32_t retval = 0;
+    sgx_status_t sgx_status = SGX_SUCCESS;
+    sgx_enclave_id_t eid = 0;
+    int launch_token_updated = 0;
+    sgx_launch_token_t launch_token = { 0 };
+
+    sgx_status = sgx_create_enclave(enclave_path,
+                                    SGX_DEBUG_FLAG,
+                                    &launch_token,
+                                    &launch_token_updated,
+                                    &eid,
+                                    NULL);
+    if (SGX_SUCCESS != sgx_status) {
+        printf("Error, call sgx_create_enclave fail [%s], SGXError:%04x.\n", __FUNCTION__, sgx_status);
+        ret = false;
+        sgx_destroy_enclave(eid);
+        return ret;
     }
 
+    sgx_status = enclave_create_report(eid,
+                                       &retval,
+                                       &qe_target_info,
+                                       app_report);
+    if ((SGX_SUCCESS != sgx_status) || (0 != retval)) {
+        printf("\nCall to get_app_enclave_report() failed\n");
+        ret = false;
+        sgx_destroy_enclave(eid);
+        return ret;
+    }
+
+    /*
     int retval = 1;
     uint8_t* pem_key = NULL;
     size_t key_size = 0;
     uint8_t* remote_report = NULL;
     size_t remote_report_size = 0;
     sgx_status_t status = get_remote_report_with_pubkey(global_eid, &retval, &pem_key, &key_size, &remote_report, &remote_report_size);
+    */
 
-    // TODO 1 [olkroshk]: check status
-
-    // TODO 2 [olkroshk]: implement the following
-// 1. parse the report/quote
-// 2. writetojson
-/*
-    myprintf("Host: Parsing the generated report and writing to a local file\n");
-    result = oe_parse_report(remote_report, remote_report_size, &parsed_report);
-    if (result != OE_OK)
-    {
-        myprintf(
-            "Host: oe_parse_report failed. %s\n",
-            oe_result_str(result));
-        if (ret == 0)
-            ret = 1;
-        goto exit;
-    }
-    else
-    {
-        QuoteFile  myQuoteFile (parsed_report, remote_report, remote_report_size, pem_key, pem_key_size);
-
-        printf("    JSON file created: %s\n", argv[2]);
-        myQuoteFile.WriteToJsonFile("./quotes", argv[2]);
-        if (EnableVerbosePrintf) 
-        {
-            myQuoteFile.WriteToJsonFile(stdout);
-        }
-    }
-*/
-    retval = 0;
-
-    /* Destroy the enclave */
-    sgx_destroy_enclave(global_eid);
-    
-    return 0;
+    sgx_destroy_enclave(eid);
+    return ret;
 }
-
-/* Initialize the enclave:
- *   Step 1: try to retrieve the launch token saved by last transaction
- *   XXX [olkroshk]: is the token necessary?
- *   Step 2: call sgx_create_enclave to initialize an enclave instance
- *   Step 3: save the launch token if it is updated
- */
-int initialize_enclave(const char* enclave_path)
-{
-    char token_path[MAX_PATH] = {'\0'};
-    sgx_launch_token_t token = {0};
-    sgx_status_t ret = SGX_ERROR_UNEXPECTED;
-    int updated = 0;
-    
-    /* Step 1: try to retrieve the launch token saved by last transaction 
-     *         if there is no token, then create a new one.
-     */
-    /* try to get the token saved in $HOME */
-    const char *home_dir = getpwuid(getuid())->pw_dir;
-    
-    if (home_dir != NULL && 
-        (strlen(home_dir)+strlen("/")+sizeof(TOKEN_FILENAME)+1) <= MAX_PATH) {
-        /* compose the token path */
-        strncpy(token_path, home_dir, strlen(home_dir));
-        strncat(token_path, "/", strlen("/"));
-        strncat(token_path, TOKEN_FILENAME, sizeof(TOKEN_FILENAME)+1);
-    } else {
-        /* if token path is too long or $HOME is NULL */
-        strncpy(token_path, TOKEN_FILENAME, sizeof(TOKEN_FILENAME));
-    }
-
-    FILE *fp = fopen(token_path, "rb");
-    if (fp == NULL && (fp = fopen(token_path, "wb")) == NULL) {
-        printf("Warning: Failed to create/open the launch token file \"%s\".\n", token_path);
-    }
-
-    if (fp != NULL) {
-        /* read the token from saved file */
-        size_t read_num = fread(token, 1, sizeof(sgx_launch_token_t), fp);
-        if (read_num != 0 && read_num != sizeof(sgx_launch_token_t)) {
-            /* if token is invalid, clear the buffer */
-            memset(&token, 0x0, sizeof(sgx_launch_token_t));
-            printf("Warning: Invalid launch token read from \"%s\".\n", token_path);
-        }
-    }
-    /* Step 2: call sgx_create_enclave to initialize an enclave instance */
-    /* Debug Support: set 2nd parameter to 1 */
-    ret = sgx_create_enclave(enclave_path, SGX_DEBUG_FLAG, &token, &updated, &global_eid, NULL);
-    if (ret != SGX_SUCCESS) {
-        print_error_message(ret);
-        if (fp != NULL) fclose(fp);
-        return -1;
-    }
-
-    /* Step 3: save the launch token if it is updated */
-    if (updated == 0 || fp == NULL) {
-        /* if the token is not updated, or file handler is invalid, do not perform saving */
-        if (fp != NULL) fclose(fp);
-        return 0;
-    }
-
-    /* reopen the file with write capablity */
-    fp = freopen(token_path, "wb", fp);
-    if (fp == NULL) return 0;
-    size_t write_num = fwrite(token, 1, sizeof(sgx_launch_token_t), fp);
-    if (write_num != sizeof(sgx_launch_token_t))
-        printf("Warning: Failed to save launch token to \"%s\".\n", token_path);
-    fclose(fp);
-    return 0;
-}
-
-
-void myprintf(const char *format, ...)
-{
-    va_list ap;
-    va_start(ap, format);
-    if (enable_verbose) {
-      printf(format, ap);
-    }
-    va_end(ap);
-}
-
-/* Check error conditions for loading enclave */
-void print_error_message(sgx_status_t ret)
-{
-    size_t idx = 0;
-    size_t ttl = sizeof sgx_errlist/sizeof sgx_errlist[0];
-
-    for (idx = 0; idx < ttl; idx++) {
-        if(ret == sgx_errlist[idx].err) {
-            if(NULL != sgx_errlist[idx].sug)
-                myprintf("Info: %s\n", sgx_errlist[idx].sug);
-            myprintf("Error: %s\n", sgx_errlist[idx].msg);
-            break;
-        }
-    }
-    
-    if (idx == ttl)
-    	myprintf("Error code is 0x%X. Please refer to the \"Intel SGX SDK Developer Reference\" for more details.\n", ret);
-}
-
-
